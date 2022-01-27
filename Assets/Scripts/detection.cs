1224f8c855bcecfb2ec3fe84871daec7195cfae9
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-01-28 01:56:39 +0530

    Resolved moving platform error
    The error that the player was not moving along with the moving platform was resolved via simply adding movement to the character itself

--- Assets/Scripts/detection.cs
+++ Assets/Scripts/detection.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class detection : MonoBehaviour
 {
     bool isGrounded = false;
-
+    [SerializeField] GameObject gb;
     private void OnTriggerStay2D(Collider2D col)
     {
+        if(col.gameObject.tag=="moving_land")
+        {
+            gb.transform.position = new Vector3(gb.transform.position.x + Time.deltaTime, gb.transform.position.y, gb.transform.position.z);
+        }
         isGrounded = true;
     }
     private void OnTriggerExit2D(Collider2D col)
