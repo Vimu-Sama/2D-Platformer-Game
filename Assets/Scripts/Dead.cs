a52acc5992dc9997475ab1f27d27e4a655dca0f7
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-01-28 19:22:10 +0530

    now, death loads the same scene again
    added  the same component to both the scenes

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -5,17 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class Dead : MonoBehaviour
 {
-
+    bool load_scene = false;
+    
+    //[SerializeField] Canvas canvas;
+    [SerializeField] Animator an;
+    [SerializeField] float del ;
+    float time;
     private void Start()
     {
         an.SetBool("already_dead", false);
+        time = 0f;
     }
-    //[SerializeField] Canvas canvas;
-    [SerializeField] Animator an;
+
+
+    private void Update()
+    {
+        if(load_scene)
+        {
+            time += Time.deltaTime;
+        }
+        if(time>=del)
+        {
+            //Debug.Log("worked here!");
+            Scene s = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(s.name);
+        }
+        
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Game Over!!!");
         an.SetBool("dead", true);
+        load_scene = true;
+        
     }
     public void reverse()
     {
