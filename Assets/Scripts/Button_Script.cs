970c9ff821c450cab418af7f54fc7fc56450fca0
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-02-04 17:58:05 +0530

    Build a lobby scene.
    1. Implemented Play and Quit Buttons into the lobby scene
    2. After clicking the play button player enters into level 1.

---
+++ Assets/Scripts/Button_Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Script : MonoBehaviour
+{
+    public void JumpToScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void Exit_Game()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game");
+    }
+
+}
